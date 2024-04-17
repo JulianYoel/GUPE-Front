461dfabcfd7d81fdfb2b398959051c57dd83d39f
JulianYoel <103387848+JulianYoel@users.noreply.github.com>
2024-04-17 07:37:08 -0300

    ..

--- script.js
+++ script.js
@@ -58,4 +58,18 @@ function switchForm() {
     alert("Iniciando sesión con Google...");
   }
   
-  
+  //menu
+  jQuery(document).ready(function($) {
+    (function ($) {
+        $(".js-nav-menu-toggle").on("click", function () {
+            $(this).parents(".navigation-menu").toggleClass("navigation-menu--open");
+        });
+
+        $("html").on("click", function (e) {
+            if (!$(e.target).closest(".js-nav-menu").length &&
+                $(".js-nav-menu").hasClass("navigation-menu--open")) {
+                $(".js-nav-menu").removeClass("navigation-menu--open");
+            }
+        });
+    })(jQuery);
+});
